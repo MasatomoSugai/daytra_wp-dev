d4627b50adf088de4e949285a970977dbc8bd2d2
Masatomo Sugai <tech.masatomo.s@gmail.com>
2021-05-27 17:58:25 +0900

    footer headerの分離

--- wp-content/themes/tf30/footer.php
+++ wp-content/themes/tf30/footer.php
@@ -1,1 +1,19 @@
 
+	<!-- footer -->
+	<footer id="footer">
+		<div class="inner">
+			<div class="copy">&copy; daily-trial WordPress theme All rights reserved.</div><!-- /copy -->
+			<div class="by">Presented by <a href="https://tokyofreelance.jp/" rel="noopener" target="_blank">東京フリーランス</a>
+			</div><!-- /by -->
+
+		</div><!-- /inner -->
+	</footer><!-- /footer -->
+
+	<div class="floating">
+		<a href="#"><i class="fas fa-chevron-up"></i></a>
+	</div>
+
+  <?php wp_footer(); ?>
+</body>
+
+</html>
