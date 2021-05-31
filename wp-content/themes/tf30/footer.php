5560f97a6baa117f19da3963e0c4d45bf51dc34c
Masatomo Sugai <tech.masatomo.s@gmail.com>
2021-05-31 12:08:44 +0900

    シェアボタンのリンクやタイトルを動的に変更

--- wp-content/themes/tf30/footer.php
+++ wp-content/themes/tf30/footer.php
@@ -7,16 +7,16 @@
 			<nav class="footer-sns-buttons">
 				<ul>
 					<li><a class="m_twitter"
-							href="https://twitter.com/share?url=https://example.com/archive/123/&text=記事のタイトルが入ります" rel="nofollow"
+							href="https://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php the_time(); ?>" rel="nofollow"
 							target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/icon-twitter.png" alt=""></a></li>
-					<li><a class="m_facebook" href="https://www.facebook.com/share.php?u=https://example.com/archive/123/"
-							rel="nofollow" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/icon-facebook.png" alt=""></a></li>
+					<li><a class="m_facebook" href="https://www.facebook.com/share.php?u=<?php the_permalink(); ?>"
+							rel="nofollow" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() . '/img/icon-facebook.png' ); ?>" alt=""></a></li>
 					<li><a class="m_hatena"
-							href="https://b.hatena.ne.jp/add?mode=confirm&url=https://example.com/archive/123/&title=記事のタイトルが入ります"
+							href="https://b.hatena.ne.jp/add?mode=confirm&url=<?php the_permalink(); ?>&title=<?php the_time(); ?>"
 							rel="nofollow" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/icon-hatena.png" alt=""></a></li>
-					<li><a class="m_line" href="https://social-plugins.line.me/lineit/share?url=https://example.com/archive/123/"
+					<li><a class="m_line" href="https://social-plugins.line.me/lineit/share?url=<?php the_permalink(); ?>"
 							rel="nofollow" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/icon-line.png" alt=""></a></li>
-					<li><a class="m_pocket" href="https://getpocket.com/edit?url=https://example.com/archive/123/" rel="nofollow"
+					<li><a class="m_pocket" href="https://getpocket.com/edit?url=<?php the_permalink(); ?>" rel="nofollow"
 							target="_blank"><img src="<?php echo get_template_directory_uri() ?>/img/icon-pocket.png" alt=""></a></li>
 				</ul>
 			</nav><!-- /footer-sns-buttons -->
