5b8c478e5a0d8678c65d9c3b76dd85e4a51eae0f
Masatomo Sugai <tech.masatomo.s@gmail.com>
2021-05-31 09:29:45 +0900

    タグの表示

--- wp-content/themes/tf30/single.php
+++ wp-content/themes/tf30/single.php
@@ -64,12 +64,15 @@
 					</div><!-- /entry-body -->
 
 
-					<div class="entry-tag-items">
-						<div class="entry-tag-head">タグ</div><!-- /entry-tag-head -->
-						<div class="entry-tag-item"><a href="">WordPress</a></div><!-- /entry-tag-item -->
-						<div class="entry-tag-item"><a href="">コーディング</a></div><!-- /entry-tag-item -->
-						<div class="entry-tag-item"><a href="">フリーランス</a></div><!-- /entry-tag-item -->
-					</div><!-- /entry-tag-items -->
+          <?php $post_tags = get_the_tags(); ?>
+            <div class="entry-tag-items">
+              <div class="entry-tag-head">タグ</div><!-- /entry-tag-head -->
+                <?php if ( $post_tags ) : ?>
+                  <?php foreach ($post_tags as $tag) : ?>
+                    <div class="entry-tag-item"><a href="<?php echo esc_url(get_tag_link($tag->term_id)); ?>"><?php echo esc_html($tag->name); ?></a></div><!-- /entry-tag-item -->
+                  <?php endforeach; ?>
+                <?php endif; ?>
+            </div><!-- /entry-tag-items -->
 
 
 					<div class="entry-related">
