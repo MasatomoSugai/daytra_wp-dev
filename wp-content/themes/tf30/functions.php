18d77b1bc12745d9bdcdd8249ef13c5115009bb4
Masatomo Sugai <tech.masatomo.s@gmail.com>
2021-06-02 12:36:04 +0900

    タグ取得を関数化

--- wp-content/themes/tf30/functions.php
+++ wp-content/themes/tf30/functions.php
@@ -126,5 +126,33 @@ function my_the_post_category($anchor = true, $id = 0) {
   }
 }
 
+// タグ取得の関数
+
+/**
+* タグ取得
+*
+* @param integer $id 投稿id.
+* @return void
+*/
+
+function my_get_post_tags($id =0) {
+  global $post;
+  // 引数が渡されなければ投稿IDをみるように設定
+  if (0 === $id) {
+    $id = $post->ID;
+  }
+
+  // タグ一覧を取得
+  $tags = get_the_tags($id);
+  
+
+  if ($tags) {
+    foreach ($tags as $tag) {
+      echo '<div class="entry-tag-item"><a href="' . esc_url(get_tag_link($tag->term_id)) . '">'. esc_html($tag->name) . '</a></div><!-- /entry-tag-item -->';
+    }
+  }
+}
+
+
 
 ?>
--- wp-content/themes/tf30/single.php
+++ wp-content/themes/tf30/single.php
@@ -65,11 +65,7 @@
           <?php $post_tags = get_the_tags(); ?>
             <div class="entry-tag-items">
               <div class="entry-tag-head">タグ</div><!-- /entry-tag-head -->
-                <?php if ( $post_tags ) : ?>
-                  <?php foreach ($post_tags as $tag) : ?>
-                    <div class="entry-tag-item"><a href="<?php echo esc_url(get_tag_link($tag->term_id)); ?>"><?php echo esc_html($tag->name); ?></a></div><!-- /entry-tag-item -->
-                  <?php endforeach; ?>
-                <?php endif; ?>
+              <?php my_get_post_tags(); ?>
             </div><!-- /entry-tag-items -->
             <!-- 関連記事 -->
         <?php get_template_part('template-parts/related-items'); ?>
