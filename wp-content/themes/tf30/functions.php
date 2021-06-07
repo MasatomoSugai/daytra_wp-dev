55c8920df855e4d7d6ca02144d8e76144fd5b8fa
Masatomo Sugai <tech.masatomo.s@gmail.com>
2021-06-08 00:20:23 +0900

    highlight.jsの読み込み

--- wp-content/themes/tf30/functions.php
+++ wp-content/themes/tf30/functions.php
@@ -38,6 +38,8 @@ add_action('after_setup_theme', 'my_setup');
 function my_script_init(){
   wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.8.2/css/all.css', array(), '5.8.2', 'all');
   wp_enqueue_style('my', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');
+  wp_enqueue_style('hl-css', get_template_directory_uri() . '/css/tomorrow-night-blue.min.css', array(), '11.0.1', 'all');
+  wp_enqueue_script('hl-js', get_template_directory_uri() . '/js/highlight.min.js', array('jquery'), '11.0.1', true);
   wp_enqueue_script('my', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0.0', true);
 
    //sns.jsを追記
